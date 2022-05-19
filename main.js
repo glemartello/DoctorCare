98323afbf8ffa39e324efd663dcc476d4b8e2380
Glenda Martello <glemartello@hotmail.com>
2022-05-19 17:19:47 -0300

    included section about

--- main.js
+++ main.js
@@ -19,5 +19,5 @@ ScrollReveal({
   distance: '30px',
   duration: 700
 }).reveal(
-  '#home, #home img, #home .stats, #services, #services header, #services .card'
+  '#home, #home img, #home .stats, #services, #services header, #services .card, #about, #about header, #about content, #about img'
 )
